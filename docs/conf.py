c22319a2c776d3e4fb6e5339034155fa1f211536
Xuan Hu <i@huxuan.org>
2022-11-09 20:35:56 +0800

    Minor.

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
 from importlib import metadata
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "serious-scaffold"
-package = "serious_scaffold"
+project = "serious-scaffold"  # pylint: disable=invalid-name
+package = project.replace("-", "_")  # pylint: disable=invalid-name
 author = metadata.metadata(package)["Author"]
-copyright = f"2022, {author}"
+copyright = f"2022, {author}"  # pylint: disable=redefined-builtin
 release = metadata.version(package)
-version = ".".join(release.split(".")[:2])
+version = ".".join(release.split(".")[:2])  # pylint: disable=invalid-name
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,5 +35,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "furo"  # pylint: disable=invalid-name
 html_static_path = ["_static"]
