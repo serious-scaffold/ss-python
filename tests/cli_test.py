e3ea8372e1e1c4480d2d2e0ca6559b5472ca1966
Xuan (Sean) Hu <i@huxuan.org>
2023-08-07 14:30:03 +0800

    Make single command as subcommands with documentation. (#70)

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -6,8 +6,15 @@ from serious_scaffold.cli import app
 runner = CliRunner()
 
 
-def test_app() -> None:
+def test_cli() -> None:
     """Tests for cli."""
     result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert "Usage" in result.output
+
+
+def test_cli_run() -> None:
+    """Tests for run subcommand of the cli."""
+    result = runner.invoke(app, "run")
     assert result.exit_code == 0
     assert not result.output
