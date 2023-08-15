4ae56342f3d06c5d4b6cceb203e5f69216130161
Xuan (Sean) Hu <i@huxuan.org>
2023-08-15 18:16:28 +0800

    Rename tests to test. (#91)

--- test/cli_test.py
+++ test/cli_test.py
@@ -1,4 +1,4 @@
-"""Tests for cli."""
+"""Test for cli."""
 from typer.testing import CliRunner
 
 from serious_scaffold.cli import app
@@ -7,14 +7,14 @@ runner = CliRunner()
 
 
 def test_cli() -> None:
-    """Tests for cli."""
+    """Test for cli."""
     result = runner.invoke(app)
     assert result.exit_code == 0
     assert "Usage" in result.output
 
 
 def test_cli_run() -> None:
-    """Tests for run subcommand of the cli."""
+    """Test for run subcommand of the cli."""
     result = runner.invoke(app, "run")
     assert result.exit_code == 0
     assert not result.output
