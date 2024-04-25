d35075d3251edb9d9695e49df7fc03c9f275038b
Xuan Hu <i@huxuan.org>
2024-04-25 09:42:43 +0800

    chore: typer follow up on no_args_is_help

--- src/ss_python/cli.py
+++ src/ss_python/cli.py
@@ -13,9 +13,6 @@ def run() -> None:
 # NOTE(huxuan): callback is required for single command as a subcommand in typer.
 # And it is a convenient way to document the cli here.
 # Reference: https://typer.tiangolo.com/tutorial/commands/one-or-multiple/#one-command-and-one-callback
-# BTW, `no_args_is_help` is set to `True` to avoid error when invoking the cli with no
-# subcommands. The default behavior is strange and might change in the future.
-# Reference: https://github.com/tiangolo/typer/issues/328
 @app.callback(no_args_is_help=True)
 def main() -> None:
     """CLI for Serious Scaffold Python."""
