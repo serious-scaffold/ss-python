7095b869fe94a317109e6a89e582fc1ec09e85f5
Xuan Hu <i@huxuan.org>
2024-11-16 07:33:50 +0000

    test

--- src/ss_python/cli.py
+++ src/ss_python/cli.py
@@ -1,23 +1,14 @@
 """Command Line Interface."""
 
-import typer
+import click
 
-app = typer.Typer()
+@click.group()
+def cli():
+    """Command Line Interface."""
 
-
-@app.command()
+@cli.command()
 def run() -> None:
     """Run command."""
 
-
-# NOTE(huxuan): callback is required for single command as a subcommand in typer.
-# And it is a convenient way to document the cli here.
-# Reference: https://typer.tiangolo.com/tutorial/commands/one-or-multiple/#one-command-and-one-callback
-@app.callback(no_args_is_help=True)
-def main() -> None:
-    """CLI for Serious Scaffold Python."""
-
-
-# NOTE(huxuan): click object is used for document generation.
-# Reference: https://github.com/tiangolo/typer/issues/200#issuecomment-796485787
-typer_click_object = typer.main.get_command(app)
+if __name__ == '__main__':
+    cli()
--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,14 +1,13 @@
 """Test for cli."""
 
-from typer.testing import CliRunner
+from click.testing import CliRunner
 
 from ss_python.cli import app
-
-runner = CliRunner()
 
 
 def test_cli() -> None:
     """Test for cli."""
+    runner = CliRunner()
     result = runner.invoke(app)
     assert result.exit_code == 0
     assert "Usage" in result.output
@@ -16,6 +15,7 @@ def test_cli() -> None:
 
 def test_cli_run() -> None:
     """Test for run subcommand of the cli."""
+    runner = CliRunner()
     result = runner.invoke(app, "run")
     assert result.exit_code == 0
     assert not result.output
